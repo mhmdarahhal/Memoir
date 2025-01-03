818c4212907155ce09dde53f057c26dcabc3122d
mhmdarahhal <mhmdarahhal@gmail.com>
2025-01-03 21:45:34 +0200

    login first commit

---
+++ resources/views/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <h1>Memoir</h1>
+    <div class="loginBox">
+        <h2>Login</h2>
+
+        <form action="/login" method="POST">
+            <div class="form">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+                <br>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            </div>
+
+            <button type="submit" class="login-btn">Login</button>
+            <p class="register-link">Don't have an account? <a href="/register">Register here</a></p>
+        </form>
+
+    </div>
+</body>
+
+</html>
