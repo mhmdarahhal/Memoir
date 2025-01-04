027ae8e054e7013d34b07ada828b179beba41367
mahdijezzini <10219045@mu.edu.lb>
2025-01-04 16:45:06 +0200

    finished log in and register design

--- resources/views/register.php
+++ resources/views/register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="register.css">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Pacifico&display=swap"
         rel="stylesheet">
     <title>Welcome</title>
@@ -17,6 +17,30 @@
 
         <form action="/register" method="POST">
             <div class="form">
+                <label for="firstname">First name</label>
+                <input type="text" id="firstname" name="first name" placeholder="Enter your first name" required>
+                <br>
+                <label for="lastname">Last name</label>
+                <input type="text" id="lastname" name="last name" placeholder="Enter your last name" required>
+                <br>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required>
+                <br>
+                <label for="dateofbirth">Date of birth</label>
+                <input type="date" id="dateofbirth" name="dateofbirth" placeholder="Enter your date of birth" required>
+                <br>
+                <label for="gender" id="gender">Gender</label>
+                <div class="radio-group">
+                    <label>
+                        <input type="radio" id="male" name="gender" value="male">
+                        <span class="custom-radio"></span> Male
+                    </label>
+                    <label>
+                        <input type="radio" id="female" name="gender" value="female">
+                        <span class="custom-radio"></span> Female
+                    </label>
+                </div>
+
                 <label for="username">Username</label>
                 <input type="text" id="username" name="username" placeholder="Enter your username" required>
                 <br>
