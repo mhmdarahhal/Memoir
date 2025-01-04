bf03e84262147a6ab999c8da1a84a933f1b9d01f
mhmdarahhal <mhmdarahhal@gmail.com>
2025-01-04 14:15:51 +0200

    css change 2

--- resources/views/login.php
+++ resources/views/login.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Pacifico&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <title>Welcome</title>
 </head>
